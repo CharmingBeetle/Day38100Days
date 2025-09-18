e000c5cc8145618be3710a245ed9a2927b0c303a
CharmingBeetle <31928209-CharmingBeetle@users.noreply.replit.com>
2025-09-18 03:33:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+vowels = ["a","e","i","o","u"]
+
+myString = input("Type summat: ")
+for letter in myString:
+  
+  if letter.lower() in vowels:
+    print('\033[33m', end='') #yellow
+    
+  print(letter, end="")
+  print('\033[0m', end='') #back to default
