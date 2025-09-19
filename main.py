8adfa83eeed5ae8376395d419a6869997241151c
Dr. Nina Thomas <104301944+CharmingBeetle@users.noreply.github.com>
2025-09-19 20:21:25 +0000

    challenge

--- main.py
+++ main.py
@@ -1,10 +1,55 @@
-vowels = ["a","e","i","o","u"]
 
-myString = input("Type a sentence: ")
-for letter in myString:
+
+def colorChange(color):
+  if color == " ":
+    print('\033[0m', end='') #back to default
   
-  if letter.lower() in vowels:
-    print('\033[33m', end='') #yellow
+  elif color == "r":
+    print('\033[0;31m', end='') #red
+
+  elif color == "b":
+    print('\033[0;34m', end='') #blue
+
+  elif color == "g":
+    print('\033[0;32m', end='') #green
+
+  elif color == "p":
+    print('\033[0;35m', end='') #purple
+
+  elif color == "y":
+    print('\033[1;33m', end='') #yellow
     
+
+sentence = input("Type a sentence: ")
+for letter in sentence:
+  colorChange(letter.lower())
   print(letter, end="")
-  print('\033[0m', end='') #back to default
+print()
+
+    
+
+
+# """ ANSI color codes """
+# BLACK = "\033[0;30m"
+# RED = "\033[0;31m"
+# GREEN = "\033[0;32m"
+# BROWN = "\033[0;33m"
+# BLUE = "\033[0;34m"
+# PURPLE = "\033[0;35m"
+# CYAN = "\033[0;36m"
+# LIGHT_GRAY = "\033[0;37m"
+# DARK_GRAY = "\033[1;30m"
+# LIGHT_RED = "\033[1;31m"
+# LIGHT_GREEN = "\033[1;32m"
+# YELLOW = "\033[1;33m"
+# LIGHT_BLUE = "\033[1;34m"
+# LIGHT_PURPLE = "\033[1;35m"
+# LIGHT_CYAN = "\033[1;36m"
+# LIGHT_WHITE = "\033[1;37m"
+# BOLD = "\033[1m"
+# FAINT = "\033[2m"
+# ITALIC = "\033[3m"
+# UNDERLINE = "\033[4m"
+# BLINK = "\033[5m"
+# NEGATIVE = "\033[7m"
+# CROSSED = "\033[9m"
